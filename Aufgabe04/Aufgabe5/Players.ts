2db1af619d119e5827393da9044289357a1dabea
Annika Kussberger <37657458+AnnikaKussberger@users.noreply.github.com>
2018-05-06 10:09:40 +0200

    Aufgabe 4.2

---
+++ Aufgabe04/Aufgabe5/Players.ts
@@ -0,0 +1,16 @@
+namespace Aufgabe5 {
+    
+    //Speicherort für Spielername und Score
+    
+    export interface Player {
+
+        name: string;
+        points: number;
+    }
+
+    export interface Players {
+        [players: string]: Player;
+    }
+
+    export let players: Players = {}
+}
