4f42adf20b773090e1f504b63e4ddbeac0d2328e
Annika Kussberger <37657458+AnnikaKussberger@users.noreply.github.com>
2018-06-10 17:29:47 +0200

    6

--- Aufgabe06/Aufgabe6.1.js
+++
@@ -1,6 +0,0 @@
-var L06_Interfaces;
-(function (L06_Interfaces) {
-    L06_Interfaces.studiSimpleArray = []; //Array zum Speichern der Studi-Datens�tze
-    L06_Interfaces.studiHomoAssoc = {}; //Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
-})(L06_Interfaces || (L06_Interfaces = {}));
-//# sourceMappingURL=Aufgabe6.1.js.map
