bfd56351b09eb4d02278c8809af10e97b8b9fea0
Annika Kussberger <37657458+AnnikaKussberger@users.noreply.github.com>
2018-06-17 14:29:19 +0200

    Add files via upload

--- Aufgabe09/Canvas9.js
+++ Aufgabe09/Canvas9.js
@@ -10,90 +10,163 @@ var Canvas09;
         console.log(crc2);
         //crc2.fillStyle = "rgba(255,0,0,0.5)";
         //crc2.fillRect(50, 50, 100, 50);
-        /*drawDavidStar(300, 100);
-        drawDavidStar(100, 200);
-
-        for (let i: number = 0; i < 100; i++) {
+        drawWater(0, 400);
+        drawSand(0, 400);
+        drawSmallFish(230, 200);
+        drawBigFish(100, 300);
+        drawBubble(300, 380, 4);
+        drawOctopus(100, 500, 15);
+        /*for (let i: number = 0; i < 100; i++) {
             let x: number = Math.random() * crc2.canvas.width;
             let y: number = Math.random() * crc2.canvas.height;
             drawDavidStar(x, y);
            
         }*/
         //sand
-        crc2.beginPath();
-        crc2.fillStyle = "rgba(228,186,138,0.5)";
-        crc2.moveTo(0, 400);
-        crc2.bezierCurveTo(150, 380, 200, 380, 360, 400);
-        crc2.lineTo(360, 640);
-        crc2.lineTo(0, 640);
-        crc2.closePath();
-        //crc2.stroke();
-        crc2.fill();
+        function drawSand(_x, _y) {
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(228,186,138,0.5)";
+            crc2.moveTo(_x, _y);
+            crc2.bezierCurveTo(_x + 150, _y - 20, _x + 200, _y - 20, _x + 360, _y);
+            crc2.lineTo(_x + 360, _y + 240);
+            crc2.lineTo(_x, _y + 240);
+            crc2.closePath();
+            //crc2.stroke();
+            crc2.fill();
+        }
         //water
-        crc2.beginPath();
-        crc2.fillStyle = "rgba(71,209,144,0.5)";
-        crc2.moveTo(0, 400);
-        crc2.bezierCurveTo(150, 380, 200, 380, 360, 400);
-        crc2.lineTo(360, 0);
-        crc2.lineTo(0, 0);
-        crc2.closePath();
-        //crc2.stroke();
-        crc2.fill();
+        function drawWater(_x, _y) {
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(71,209,144,0.5)";
+            crc2.moveTo(_x, _y);
+            crc2.bezierCurveTo(_x + 150, _y - 20, _x + 200, _y - 20, _x + 360, _y);
+            crc2.lineTo(_x + 360, _y - 400);
+            crc2.lineTo(_x, _y - 400);
+            crc2.closePath();
+            //crc2.stroke();
+            crc2.fill();
+        }
         //fish big
-        crc2.beginPath();
-        crc2.moveTo(100, 300);
-        crc2.bezierCurveTo(120, 297, 130, 280, 150, 300);
-        crc2.bezierCurveTo(130, 320, 120, 303, 100, 300);
-        crc2.lineTo(90, 290);
-        crc2.lineTo(90, 310);
-        crc2.lineTo(100, 300);
-        crc2.stroke();
+        function drawBigFish(_x, _y) {
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(255, 102, 51,1)";
+            crc2.moveTo(_x, _y);
+            crc2.bezierCurveTo(_x + 20, _y - 3, _x + 30, _y - 20, _x + 50, _y);
+            crc2.bezierCurveTo(_x + 30, _y + 20, _x + 20, _y + 3, _x, _y);
+            crc2.lineTo(_x - 10, _y - 10);
+            crc2.lineTo(_x - 10, _y + 10);
+            crc2.lineTo(_x, _y);
+            crc2.fill();
+            //crc2.stroke();
+        }
         //fish small
-        crc2.beginPath();
-        crc2.moveTo(220, 200);
-        crc2.bezierCurveTo(230, 190, 245, 198, 250, 200);
-        crc2.bezierCurveTo(245, 202, 230, 210, 220, 200);
-        crc2.moveTo(250, 200);
-        crc2.lineTo(255, 195);
-        crc2.lineTo(255, 205);
-        crc2.lineTo(250, 200);
-        crc2.stroke();
+        function drawSmallFish(_x, _y) {
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(255, 51, 51,1)";
+            crc2.moveTo(_x, _y);
+            crc2.bezierCurveTo(_x + 10, _y - 10, _x + 25, _y - 2, _x + 30, _y);
+            crc2.bezierCurveTo(_x + 25, _y + 2, _x + 10, _y + 10, _x, _y);
+            crc2.moveTo(_x + 30, _y);
+            crc2.lineTo(_x + 35, _y - 5);
+            crc2.lineTo(_x + 35, _y + 5);
+            crc2.lineTo(_x + 30, _y);
+            crc2.fill();
+            //crc2.stroke();
+        }
         //bubble
-        ctx.beginPath();
-        ctx.arc(300, 380, 4, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.stroke();
+        function drawBubble(_x, _y, _radius) {
+            ctx.beginPath();
+            ctx.fillStyle = "rgba(51, 153, 255,1)";
+            ctx.arc(_x, _y, _radius, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+            //ctx.stroke();
+        }
         //Oktopus head
-        ctx.beginPath();
-        ctx.arc(100, 500, 15, 0.5 * Math.PI, 1.5 * Math.PI);
-        crc2.lineTo(120, 485);
-        ctx.arc(120, 500, 15, 1.5 * Math.PI, 0.5 * Math.PI);
-        ctx.closePath();
-        //Octopus tentacles
-        crc2.lineTo(100, 540);
-        crc2.moveTo(122, 515);
-        crc2.lineTo(122, 540);
-        crc2.moveTo(111, 515);
-        crc2.lineTo(111, 540);
-        //Octopus Eyes
-        ctx.moveTo(122, 500);
-        ctx.arc(122, 500, 1, 0, Math.PI * 2);
-        ctx.stroke();
-        ctx.moveTo(100, 500);
-        ctx.arc(100, 500, 1, 0, Math.PI * 2);
-        ctx.stroke();
+        function drawOctopus(_x, _y, _radius) {
+            ctx.beginPath();
+            ctx.fillStyle = "rgba(142, 80, 103,1)";
+            ctx.arc(_x, _y, _radius, 0.5 * Math.PI, 1.5 * Math.PI);
+            crc2.lineTo(_x + 20, _y - 15);
+            ctx.arc(_x + 20, _y, _radius, 1.5 * Math.PI, 0.5 * Math.PI);
+            ctx.fill();
+            ctx.closePath();
+            //Octopus tentacles
+            crc2.moveTo(_x, _y + 15);
+            crc2.lineTo(_x, _y + 40);
+            crc2.moveTo(_x + 22, _y + 15);
+            crc2.lineTo(_x + 22, _y + 40);
+            crc2.moveTo(_x + 11, _y + 15);
+            crc2.lineTo(_x + 11, _y + 40);
+            //Octopus Eyes
+            ctx.moveTo(_x + 22, _y);
+            ctx.arc(_x + 22, _y, 1, 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.moveTo(_x, _y);
+            ctx.arc(_x, _y, 1, 0, Math.PI * 2);
+            ctx.stroke();
+        }
         //seagras open right
         crc2.beginPath();
-        crc2.moveTo(110, 400);
-        crc2.bezierCurveTo(100, 340, 80, 309, 130, 300);
-        crc2.bezierCurveTo(100, 315, 100, 340, 120, 400);
-        crc2.stroke();
+        crc2.fillStyle = "rgba(51, 204, 0,0.5)";
+        crc2.moveTo(70, 400);
+        crc2.bezierCurveTo(60, 340, 40, 309, 90, 300);
+        crc2.bezierCurveTo(60, 315, 60, 340, 80, 400);
+        crc2.closePath();
+        crc2.fill();
+        //crc2.stroke()
         //seagras open left
         crc2.beginPath();
+        crc2.fillStyle = "rgba(51, 204, 0,0.5)";
         crc2.moveTo(240, 400);
         crc2.bezierCurveTo(260, 360, 280, 309, 230, 300);
         crc2.bezierCurveTo(265, 325, 250, 340, 225, 400);
+        crc2.closePath();
+        crc2.fill();
+        //crc2.stroke()
+        //rock
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(105,105,105,0.6)";
+        crc2.moveTo(360, 430);
+        crc2.lineTo(340, 430);
+        crc2.lineTo(310, 440);
+        crc2.lineTo(300, 450);
+        crc2.lineTo(300, 460);
+        crc2.lineTo(360, 460);
+        crc2.closePath;
+        crc2.fill();
+        //crc2.stroke();
+        //box right piece
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(128,64,0,0.6)";
+        crc2.moveTo(260, 540);
+        crc2.lineTo(260, 590);
+        crc2.lineTo(300, 570);
+        crc2.lineTo(300, 520);
+        crc2.closePath();
+        crc2.fill();
         crc2.stroke();
+        //box left piece
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(128,64,0,0.6)";
+        crc2.moveTo(260, 540);
+        crc2.lineTo(200, 530);
+        crc2.lineTo(200, 580);
+        crc2.lineTo(260, 590);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+        //cover
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(100,100,100,1)";
+        ctx.moveTo(260, 540);
+        ctx.arc(260, 520, 20, 0.5 * Math.PI, 1.5 * Math.PI);
+        ctx.lineTo(200, 490);
+        ctx.moveTo(200, 530);
+        ctx.arc(200, 510, 20, 0.5 * Math.PI, 1.5 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
     }
 })(Canvas09 || (Canvas09 = {}));
 //# sourceMappingURL=Canvas9.js.map
--- Aufgabe09/Canvas9.ts
+++ Aufgabe09/Canvas9.ts
@@ -3,7 +3,7 @@ namespace Canvas09 {
     let crc2: CanvasRenderingContext2D;
     let ctx: CanvasRenderingContext2D;
 
-    
+
 
     function init(_event: Event): void {
         let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
@@ -14,119 +14,189 @@ namespace Canvas09 {
         //crc2.fillStyle = "rgba(255,0,0,0.5)";
         //crc2.fillRect(50, 50, 100, 50);
 
-        /*drawDavidStar(300, 100);
-        drawDavidStar(100, 200);
+        drawWater(0, 400);
+        drawSand(0, 400);
+        //drawSmallFish(230, 200);
+        drawBigFish(100, 300);
+        drawBubble(300, 380, 4);
+        drawOctopus(100, 500, 15);
+        drawSeagrasOpenRight(70, 400);
+        drawSeagrasOpenLeft(240, 400);
+        drawRock(360, 430);
 
-        for (let i: number = 0; i < 100; i++) {
+
+
+        for (let i: number = 0; i < 20; i++) {
             let x: number = Math.random() * crc2.canvas.width;
             let y: number = Math.random() * crc2.canvas.height;
-            drawDavidStar(x, y);
-           
-        }*/
-        
-        //sand
+            drawSmallFish(x, y);
+
+        }
+    }
+    //sand
+    function drawSand(_x: number, _y: number): void {
         crc2.beginPath();
         crc2.fillStyle = "rgba(228,186,138,0.5)";
-        crc2.moveTo(0,400);
-        crc2.bezierCurveTo(150,380,200,380,360,400);
-        crc2.lineTo(360,640);
-        crc2.lineTo(0,640);
+        crc2.moveTo(_x, _y);
+        crc2.bezierCurveTo(_x + 150, _y - 20, _x + 200, _y - 20, _x + 360, _y);
+        crc2.lineTo(_x + 360, _y + 240);
+        crc2.lineTo(_x, _y + 240);
         crc2.closePath();
         //crc2.stroke();
         crc2.fill();
-        
-        //water
-        crc2.beginPath();
-        crc2.fillStyle = "rgba(71,209,144,0.5)";
-        crc2.moveTo(0,400);
-        crc2.bezierCurveTo(150,380,200,380,360,400);
-        crc2.lineTo(360,0);
-        crc2.lineTo(0,0);
-        crc2.closePath();
-        //crc2.stroke();
-        crc2.fill();
-        
-        //fish big
-        crc2.beginPath();
-        crc2.moveTo (100,300);
-        crc2.bezierCurveTo(120,297,130,280,150,300);
-        crc2.bezierCurveTo(130,320,120,303,100,300);
-        crc2.lineTo(90,290);
-        crc2.lineTo(90,310);
-        crc2.lineTo(100,300);
-        crc2.stroke();
-        
-        //fish small
-        crc2.beginPath();
-        crc2.moveTo (220,200);
-        crc2.bezierCurveTo(230,190,245,198,250,200);
-        crc2.bezierCurveTo(245,202,230,210,220,200);
-        crc2.moveTo(250,200);
-        crc2.lineTo(255,195);
-        crc2.lineTo(255,205);
-        crc2.lineTo(250,200);
-        crc2.stroke();
-        
-        
-        //bubble
-        ctx.beginPath();
-        ctx.arc(300, 380, 4, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.stroke();
-        
-        
-        
-        //Oktopus head
-        ctx.beginPath();
-        ctx.arc(100, 500, 15, 0.5 * Math.PI, 1.5 * Math.PI);
-        crc2.lineTo(120,485);
-        ctx.arc(120, 500, 15,  1.5 * Math.PI,0.5 * Math.PI);
-        ctx.closePath();
-       
-        //Octopus tentacles
-        crc2.lineTo(100,540);
-        crc2.moveTo(122,515);
-        crc2.lineTo(122,540);
-        crc2.moveTo(111,515);
-        crc2.lineTo(111,540);
-        
-        //Octopus Eyes
-        ctx.moveTo(122,500);
-        ctx.arc(122, 500, 1, 0, Math.PI * 2);
-        ctx.stroke();
-        ctx.moveTo(100,500);
-        ctx.arc(100, 500, 1, 0, Math.PI * 2);
-        ctx.stroke();
-        
-        //seagras open right
-        crc2.beginPath();
-        crc2.moveTo (110,400);
-        crc2.bezierCurveTo(100,340,80,309,130,300);
-        crc2.bezierCurveTo(100,315,100,340,120,400);
-        crc2.stroke()
-        
-        //seagras open left
-        crc2.beginPath();
-        crc2.moveTo (240,400);
-        crc2.bezierCurveTo(260,360,280,309,230,300);
-        crc2.bezierCurveTo(265,325,250,340,225,400);
-        crc2.stroke()
     }
 
-    /*function drawDavidStar(_x: number, _y: number): void {
-    crc2.beginPath();
-    crc2.moveTo(_x, _y - 20);
-    crc2.lineTo(_x + 20, _y + 10);
-    crc2.lineTo(_x - 20, _y + 10);
-    crc2.closePath();
+    //water
+    function drawWater(_x: number, _y: number): void {
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(71,209,144,0.5)";
+        crc2.moveTo(_x, _y);
+        crc2.bezierCurveTo(_x + 150, _y - 20, _x + 200, _y - 20, _x + 360, _y);
+        crc2.lineTo(_x + 360, _y - 400);
+        crc2.lineTo(_x, _y - 400);
+        crc2.closePath();
+        //crc2.stroke();
+        crc2.fill();
+    }
 
-    crc2.moveTo(_x, _y + 20);
-    crc2.lineTo(_x + 20, _y - 10);
-    crc2.lineTo(_x - 20, _y - 10);
-    crc2.closePath();
-    crc2.stroke();
-    crc2.fill();
-}*/
-  
-    
-}
+    //fish big
+    function drawBigFish(_x: number, _y: number): void {
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(255, 102, 51,1)";
+        crc2.moveTo(_x, _y);
+        crc2.bezierCurveTo(_x + 20, _y - 3, _x + 30, _y - 20, _x + 50, _y);
+        crc2.bezierCurveTo(_x + 30, _y + 20, _x + 20, _y + 3, _x, _y);
+        crc2.lineTo(_x - 10, _y - 10);
+        crc2.lineTo(_x - 10, _y + 10);
+        crc2.lineTo(_x, _y);
+        crc2.fill();
+        //crc2.stroke();
+    }
+
+    //fish small
+    function drawSmallFish(_x: number, _y: number): void {
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(255, 51, 51,1)";
+        crc2.moveTo(_x, _y);
+        crc2.bezierCurveTo(_x + 10, _y - 10, _x + 25, _y - 2, _x + 30, _y);
+        crc2.bezierCurveTo(_x + 25, _y + 2, _x + 10, _y + 10, _x, _y);
+        crc2.moveTo(_x + 30, _y);
+        crc2.lineTo(_x + 35, _y - 5);
+        crc2.lineTo(_x + 35, _y + 5);
+        crc2.lineTo(_x + 30, _y);
+        crc2.fill();
+        //crc2.stroke();
+    }
+
+    //bubble
+    function drawBubble(_x: number, _y: number, _radius: number): void {
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(51, 153, 255,1)";
+        ctx.arc(_x, _y, _radius, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+        //ctx.stroke();
+    }
+
+
+
+    //Oktopus head
+    function drawOctopus(_x: number, _y: number, _radius: number): void {
+        ctx.beginPath();
+        ctx.fillStyle = "rgba(142, 80, 103,1)";
+        ctx.arc(_x, _y, _radius, 0.5 * Math.PI, 1.5 * Math.PI);
+        crc2.lineTo(_x + 20, _y - 15);
+        ctx.arc(_x + 20, _y, _radius, 1.5 * Math.PI, 0.5 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+
+        //Octopus tentacles
+        crc2.moveTo(_x, _y + 15);
+        crc2.lineTo(_x, _y + 40);
+        crc2.moveTo(_x + 22, _y + 15);
+        crc2.lineTo(_x + 22, _y + 40);
+        crc2.moveTo(_x + 11, _y + 15);
+        crc2.lineTo(_x + 11, _y + 40);
+
+        //Octopus Eyes
+        ctx.moveTo(_x + 22, _y);
+        ctx.arc(_x + 22, _y, 1, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.moveTo(_x, _y);
+        ctx.arc(_x, _y, 1, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+
+
+    //seagras open right
+    function drawSeagrasOpenRight(_x: number, _y: number): void {
+        crc2.beginPath();
+        crc2.fillStyle = "rgba(51, 204, 0,0.5)";
+        crc2.moveTo(_x, _y);
+        crc2.bezierCurveTo(_x + 10, _y - 60, _x - 30, _y - 91, _y + 20, _y - 100);
+        crc2.bezierCurveTo(_x - 10, _y - 85, _x - 10, _y - 60, _x + 10, _y);
+        crc2.closePath();
+        crc2.fill();
+        //crc2.stroke    
+        //seagras open left
+        function drawSeagrasOpenLeft(_x: number, _y: number): void {
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(51, 204, 0,0.5)";
+            crc2.moveTo(_x, _y);
+            crc2.bezierCurveTo(_x + 20, _y - 40, _x + 40, _y - 91, _x - 10, _y - 100);
+            crc2.bezierCurveTo(_x + 24, _y - 75, _x + 10, _y - 60, _x - 15, _y);
+            crc2.closePath();
+            crc2.fill();
+            //crc2.st   
+                  //rock
+            function drawRock(_x: number, _y: number): void {
+                crc2.beginPath();
+                crc2.fillStyle = "rgba(105,105,105,0.6)";
+                crc2.moveTo(_x, _y);
+                crc2.lineTo(_x - 20, _y);
+                crc2.lineTo(_x - 50, _y + 10);
+                crc2.lineTo(_x - 60, _y + 20);
+                crc2.lineTo(_x - 60, _y + 30);
+                crc2.lineTo(_x, _y + 30);
+                crc2.closePath();
+                crc2.fill();
+                //crc2.str  
+            }
+            //box right piece
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(128,64,0,0.6)";
+            crc2.moveTo(260, 540);
+            crc2.lineTo(260, 590);
+            crc2.lineTo(300, 570);
+            crc2.lineTo(300, 520);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            //box left piece
+            crc2.beginPath();
+            crc2.fillStyle = "rgba(128,64,0,0.6)";
+            crc2.moveTo(260, 540);
+            crc2.lineTo(200, 530);
+            crc2.lineTo(200, 580);
+            crc2.lineTo(260, 590);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+
+            //cover
+            ctx.beginPath();
+            ctx.fillStyle = "rgba(100,100,100,1)";
+            ctx.moveTo(260, 540);
+            ctx.arc(260, 520, 20, 0.5 * Math.PI, 1.5 * Math.PI);
+            ctx.lineTo(200, 490);
+            ctx.moveTo(200, 530);
+            ctx.arc(200, 510, 20, 0.5 * Math.PI, 1.5 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
+            ctx.closePath();
+
+
+
+        }
+
