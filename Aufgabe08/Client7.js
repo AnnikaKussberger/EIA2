78c9e8c49044924eb6d9766f0b2a7878e1fd6b27
Annika Kussberger <37657458+AnnikaKussberger@users.noreply.github.com>
2018-06-21 11:49:40 +0200

    7

--- Aufgabe08/Client7.js
+++ Aufgabe08/Client7.js
@@ -1,1 +1,93 @@
+var L07_Interfaces;
+(function (L07_Interfaces) {
+    window.addEventListener("load", init);
+    let address = "https://eia2node257455.herokuapp.com/";
+    let inputs = document.getElementsByTagName("input");
+    function init() {
+        console.log("Init");
+        //Enventlistener auf Button �bergeben
+        let insertButton = document.getElementById("insert");
+        let searchButton = document.getElementById("search");
+        let refreshButton = document.getElementById("refresh");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+        searchButton.addEventListener("click", search);
+    }
+    L07_Interfaces.init = init;
+    // Funktion um Daten der Studenten zu speichern
+    function insert(_event) {
+        let genderButton = document.getElementById("male");
+        let matrikel = inputs[2].value;
+        let studi;
+        //Interface �bergeben
+        studi = {
+            name: inputs[0].value,
+            firstname: inputs[1].value,
+            matrikel: parseInt(matrikel),
+            age: parseInt(inputs[3].value),
+            gender: genderButton.checked,
+            studiengang: document.getElementsByTagName("select").item(0).value,
+        };
+        let stringifyJSON = JSON.stringify(studi); //studi wird in einen Sting umgewandelt
+        console.log(stringifyJSON);
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=insert&data=" + stringifyJSON, true);
+        xhr.send(); //Request wird an den Server geschickt
+    }
+    function handleChangeInsert(_event) {
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response); //Wenn Operation abgeschlossen wurde, gebe die Antwort wieder
+        }
+    }
+    //Funktion zu Aufrufen der Daten
+    function refresh(_event) {
+        let xhr = new XMLHttpRequest(); //Neue Request wird erstellt
+        xhr.open("GET", address + "?command=findAll", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                let studis = JSON.parse(xhr.responseText); //Umwandlung des Antwort in ein Objekt
+                console.log(studis);
+                let answer = "";
+                for (let i = 0; i < studis.length; i++) {
+                    answer += "Name: " + studis[i].name + ", " + studis[i].firstname + ", Matrikel: " + studis[i].matrikel + ", "
+                        + studis[i].studiengang + ", Mann: " + studis[i].gender + ", Alter: " + studis[i].age + "\n";
+                }
+                document.getElementsByTagName("textarea")[1].value = answer; //Im zweiten Textfeld werden die Antworten wiedergegeben
+            }
+        };
+        xhr.send(); //Request wird gesendet
+    }
+    function handleChangeRefresh(_event) {
+        let output = document.getElementsByTagName("textarea")[1];
+        output.value = "";
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
+    function search(_event) {
+        let matrikel = inputs[6].value;
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=find&data=" + matrikel, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                let studi = JSON.parse(xhr.responseText); //Umwandlung des Antwort in ein Objekt
+                console.log(studi);
+                let answer = "Name: " + studi.name + ", " + studi.firstname + ", Matrikel: " + studi.matrikel + ", "
+                    + studi.studiengang + ", Mann: " + studi.gender + ", Alter: " + studi.age + "\n";
+                document.getElementsByTagName("textarea")[0].value = answer; //Im ersten Testfeld erscheint die Antwort
+            }
+        };
+        xhr.send(); //Request wird gesendet
+    }
+    function handleChangeSearch(_event) {
+        let output = document.getElementsByTagName("textarea")[0];
+        output.value = "";
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
+})(L07_Interfaces || (L07_Interfaces = {}));
 //# sourceMappingURL=Client7.js.map
